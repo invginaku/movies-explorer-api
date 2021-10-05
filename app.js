30782fd194c57ae8098d036a229c916be4f0029d
Nikita Ivochkin <70746527+invginaku@users.noreply.github.com>
2021-10-05 22:35:26 +0300

    Добавление cors

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
@@ -25,9 +26,26 @@ mongoose.connect(DATA_BASE, {
   useFindAndModify: false,
 });
 
+const corsWhiteList = [
+  'https://inmovies.nomoredomains.club',
+  'http://inmovies.nomoredomains.club',
+  'https://localhost:3000',
+  'http://localhost:3000',
+];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (corsWhiteList.indexOf(origin) !== -1) {
+      callback(null, true);
+    }
+  },
+  credentials: true,
+};
+
 app.use(requestLogger);
 app.use(cookieParser());
 app.use(limit);
+app.use(cors(corsOptions));
 app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
